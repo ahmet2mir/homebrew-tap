35af53b1e4cc0b6a90296e4f2556199fe48d8b80
Ahmet DEMIR <me@ahmet2mir.eu>
2020-10-28 10:06:05 +0100

    bump v1.1.8

--- Formula/wq.rb
+++ Formula/wq.rb
@@ -3,16 +3,16 @@
 class Wq < Formula
   desc "Command-line TOML/JSON/INI/YAML/XML/HCL processor using jq c bindings"
   homepage "https://github.com/ahmet2mir/wildq"
-  version "1.1.5"
+  version "1.1.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ahmet2mir/wildq/releases/download/v1.1.5/wildq-1.1.5-darwin-x86_64.tar.gz"
-    sha256 "9c19e42f1c7b0a71f0bc87136e923017e6b96850bfd7dffc603732513dadd23a"
+    url "https://github.com/ahmet2mir/wildq/releases/download/v1.1.8/wildq-1.1.8-darwin-x86_64.tar.gz"
+    sha256 "99ce7981b7ef3b9f668fd40f9d8de6bc38c367cb7ed1a02b7744ecc9218c6cfc"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ahmet2mir/wildq/releases/download/v1.1.5/wildq-1.1.5-linux-x86_64.tar.gz"
-      sha256 "15c382f2376f62ee44e119ab702d04cc94c7a238d8372c3afa49d68664eb8803"
+      url "https://github.com/ahmet2mir/wildq/releases/download/v1.1.8/wildq-1.1.8-linux-x86_64.tar.gz"
+      sha256 "1bac8037ecd183497df1f5c2f2ae951de10c6242e8ec577df903e66449c9f426"
     end
   end
 
