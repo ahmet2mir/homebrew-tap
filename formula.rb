6fd65f5775c4049c20b500db85190fdf31103bf5
Ahmet Demir <me@ahmet2mir.eu>
2022-09-02 15:23:49 +0200

    fix: remove "bottle :unneeded" as it is deprecated

--- formula.rb
+++ formula.rb
@@ -4,7 +4,6 @@ class {{ CLASS_NAME }} < Formula
   desc "Command-line TOML/JSON/INI/YAML/XML/HCL processor using jq c bindings"
   homepage "https://github.com/ahmet2mir/wildq"
   version "{{ VERSION }}"
-  bottle :unneeded
 
   if OS.mac?
     url "https://github.com/ahmet2mir/wildq/releases/download/v{{ VERSION }}/wildq-{{ VERSION }}-darwin-x86_64.tar.gz"
